a85e90e4f9f1fed43acbcbc5784cb7519b3585b6
Артемчук Константин <atremchuk@gmail.com>
2022-03-31 15:09:33 +0300

    mean, min and max price, json output

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Sugar tracker
 
 This program allows you to track the price of 1 kg of sugar in Russia, based on sbermegamarket.ru data.
+
+## Usage
+
+Install the requirements and just run `sugar_tracker.py`. Add the `-j` or `--json` argument to get JSON output.
+
+## Example
+
+```
+> python ./sugar_tracker.py   
+Prices per 1 kg according to https://sbermegamarket.ru/catalog/sahar/ data:
+mean: 102.64 ₽
+min: 85.0 ₽ (Сахар песок белый Продимекс 1 кг)
+max: 121.11 ₽ (Сахар Rioba свекловичный белый песок 900 г
+
+> python ./sugar_tracker.py --json
+{
+  "mean_price": 102.64,
+  "min_price": 85.0,
+  "min_name": "Сахар песок белый Продимекс 1 кг",
+  "max_price": 121.11,
+  "max_name": "Сахар Rioba свекловичный белый песок 900 г",
+  "source": "https://sbermegamarket.ru/catalog/sahar/"
+}
+```
--- sugar_tracker.py
+++ sugar_tracker.py
@@ -1,26 +1,64 @@
+# 2022, KizhiFox (https://github.com/KizhiFox/)
+# This program is distributed under the GNU General Public License.
 import requests
+import json
+from sys import argv
 from bs4 import BeautifulSoup
 
 
 SUGAR_PAGE = 'https://sbermegamarket.ru/catalog/sahar/'
 KEYWORDS = ['сахар', 'песок']
+EXCLUDE = ['тростниковый']
+
+JSON_OUTPUT = any(arg in ['-j', '--json'] for arg in argv)
 
 
 res = requests.get(SUGAR_PAGE)
 res.encoding = 'utf-8'
 soup = BeautifulSoup(res.text, 'html.parser')
 
-sugar = None
+sugar = []
 for item in soup.findAll('div', {'class': 'item-block'}):
     item_name = item.find('a', {'class': 'ddl_product_link'})
     if item_name:
-        if set(KEYWORDS).issubset(item_name.text.lower().split()):
-            sugar = item
-            break
+        item_name = item_name.text.lower().split()
+        if set(KEYWORDS).issubset(item_name) and all(exc not in item_name for exc in EXCLUDE):
+            sugar.append(item)
 
-if sugar is None:
-    print('No sugar available :(')
+if len(sugar) > 0:
+    sugar_prices = []
+    sugar_names = []
+    for s in sugar:
+        price = float(s.find('div', {'class': 'item-price'}).text.replace(',', '.').split()[0])
+        weight = float(s.find('p', {'class': 'item-details-item__value'}).text.replace(',', '.').split()[-1]) / 1000
+        sugar_prices.append(round(price / weight, 2))
+        sugar_names.append(s.find('a', {'class': 'ddl_product_link'}).text)
+
+    min_price = min(sugar_prices)
+    min_name = sugar_names[sugar_prices.index(min_price)]
+
+    max_price = max(sugar_prices)
+    max_name = sugar_names[sugar_prices.index(max_price)]
+
+    mean_price = round(sum(sugar_prices) / len(sugar_prices), 2)
+
+    if JSON_OUTPUT:
+        print(json.dumps({'mean_price': mean_price,
+                          'min_price': min_price,
+                          'min_name': min_name,
+                          'max_price': max_price,
+                          'max_name': max_name,
+                          'source': SUGAR_PAGE},
+                         ensure_ascii=False, indent=2))
+
+    else:
+        print(f'Prices per 1 kg according to {SUGAR_PAGE} data:\n'
+              f'mean: {mean_price} ₽\n'
+              f'min: {min_price} ₽ ({min_name})\n'
+              f'max: {max_price} ₽ ({max_name})')
+
 else:
-    price = float(sugar.find('div', {'class': 'item-price'}).text.replace(',', '.').split()[0])
-    weight = float(sugar.find('p', {'class': 'item-details-item__value'}).text.replace(',', '.').split()[-1]) / 1000
-    print(round(price / weight, 2))
+    if JSON_OUTPUT:
+        print('{"exception": "No sugar available :("}')
+    else:
+        print('No sugar available :(')
